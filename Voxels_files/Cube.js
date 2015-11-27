a2df10c987cb6fedb049521fac67234f9c2c6586
jjspacer <thesantacruzian@gmail.com>
2015-11-27 15:51:18 -0800

    change Plane to be simpler

--- Voxels_files/Cube.js
+++ Voxels_files/Cube.js
@@ -1,9 +1,3 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-
 var Cube = (function(_super){
 	__extends(Cube, _super);
 	function Cube(width, height, depth, theta){
